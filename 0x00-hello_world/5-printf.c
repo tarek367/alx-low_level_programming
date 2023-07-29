2a88fe070921cfd5bd49974babe1b99984332754
tarek367 <tareknanna838@gmail.com>
2023-07-29 14:55:36 -0700

    a

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	/* Print the desired string with proper grammar,
+	   but the outcome is a piece of art, followed by a new line */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	/* Return 0 to indicate successful execution */
+	return (0);
+}
