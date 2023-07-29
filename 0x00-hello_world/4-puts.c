b6e19a41fd4d83e925ad9d67fcdb4e16212ea61f
tarek367 <tareknanna838@gmail.com>
2023-07-29 14:45:20 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,12 +4,9 @@
  * main - Entry point of the program
  *
  * Return: Always 0 (Success)
- */
+*/
 int main(void)
 {
-    /* Print the desired string using puts function */
-    puts("Programming is like building a multilingual puzzle");
-
-    /* Return 0 to indicate successful execution */
+    puts("\"Programming is like building a multilingual puzzle");
     return (0);
 }
