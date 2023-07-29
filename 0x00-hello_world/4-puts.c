64d53207b7e770ba7fd2c9710a238546f704591b
tarek367 <tareknanna838@gmail.com>
2023-07-29 14:25:37 -0700

    a

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    /* Print the desired string using puts function */
+    puts("Programming is like building a multilingual puzzle");
+
+    /* Return 0 to indicate successful execution */
+    return (0);
+}
